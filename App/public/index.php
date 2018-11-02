821ead66660f2c65752b6da13c7f7ade68d6c183
AlexandreT <tual.alexandre@gmail.com>
2018-11-02 17:44:21 +0100

    use Autoloader and router run method on index.php

--- App/public/index.php
+++ App/public/index.php
@@ -6,3 +6,13 @@
  * Time: 16:39
  */
 
+use App\config\Router;
+use App\config\Autoloader;
+
+define('ROOT', dirname(__DIR__));
+
+require ROOT.'/config/Autoloader.php';
+Autoloader::register();
+
+$router = new Router();
+$router->run();
